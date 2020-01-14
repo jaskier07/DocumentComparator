7501092d35e4a93e3a32c649df6522e8871eeefe
Mat J <yooz3k35@gmail.com>
2020-01-14 22:58:32 +0100

    Tweaks to graph visualization

--- GraphDrawer.py
+++ GraphDrawer.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 class GraphDrawer:
 
     def draw(self, array, filenames):
-        g = self.__getGraph(array, filenames)
+        g = self.__getGraph(array, self.__prepareFilenames(filenames))
 
         plt.figure(figsize=(10, 10))
 
@@ -13,7 +13,7 @@ class GraphDrawer:
         edges = g.edges()
         weights = [g[u][v]['weight'] * 10 for u, v in edges]
 
-        nx.draw(g, pos, with_labels=True, node_size=400, node_color="skyblue", node_shape=".", font_size=11,
+        nx.draw(g, pos, with_labels=True, node_size=1200, node_color="skyblue", node_shape="h", font_size=11, alpha=0.8,
                 width=weights)
 
         weights = [round(g[u][v]['weight'], 4) for u, v in edges]
@@ -28,3 +28,12 @@ class GraphDrawer:
             for j in range(i + 1, len(array[0])):
                 g.add_edge(titles[i], titles[j], weight=array[i][j])
         return g
+
+    def __prepareFilenames(self, filenames):
+        shortFilenames = []
+        for i in range(len(filenames)):
+            max_len = 25
+            name = filenames[i][:max_len] + '...' if len(filenames[i]) > max_len else filenames[i]
+            shortFilenames.append(name)
+
+        return shortFilenames
