5e533d1178e7461aa4f1403a9f5ec1de7cecf8a7
Mat J <yooz3k35@gmail.com>
2020-01-15 21:38:50 +0100

    Getting rid of embedded image

--- Application.py
+++ Application.py
@@ -25,17 +25,7 @@ def compare_documents(paths_to_pdf_files, pdf_names):
     label_info_progressbar['text'] = 'Comparing completed.'
 
     drawer = GraphDrawer()
-    drawer.draw(arr, pdf_names, True)
-
-    im = Image.open("appdata/compare_result.png")
-    resized = im.resize((520, 520), Image.ANTIALIAS)
-    photo = ImageTk.PhotoImage(resized)
-    img_placeholder['image'] = photo
-    img_placeholder.image = photo
-
-    button_open_in_new_window = Button(text="Open image in new window",
-                                       command=partial(drawer.draw, arr, pdf_names, False))
-    button_open_in_new_window.grid(row=0, column=1)
+    drawer.draw(arr, pdf_names)
 
 
 def browse_button():
@@ -45,7 +35,7 @@ def browse_button():
     label_header_info['text'] = 'PDFs found in directory:'
     button_compare_documents = Button(text="Compare documents",
                                       command=partial(compare_documents, paths_to_pdf_files, pdf_names))
-    button_compare_documents.grid(row=2, column=0, padx=(5, 5))
+    button_compare_documents.grid(row=1, column=1, sticky="NW", padx=(5, 5))
 
 
 def configure_styles():
@@ -56,7 +46,7 @@ def configure_styles():
 
 window = Tk()
 window.title("Document comparator")
-window.geometry('1200x640')
+window.geometry('800x480')
 
 configure_styles()
 
@@ -69,8 +59,5 @@ label_content.grid(row=1, column=0, sticky="NW", padx=(5, 5))
 
 button_browse = Button(text="Browse for directory", command=browse_button)
 button_browse.grid(row=0, column=1, sticky="W", padx=(5, 5))
-
-img_placeholder = Label(window)
-img_placeholder.grid(row=1, column=1)
 
 window.mainloop()
